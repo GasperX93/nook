268b010f370cc84351f16efb3b85d744c948037b
Gasper Zupan <gasper_zupan@yahoo.com>
2026-03-24 07:00:21 +0100

    fix: remove use-postage-snapshot from default config
    Bee v2.7.1 removed this flag entirely. The migration handles existing
    users, but new installs were still getting it in the default config.yaml.

--- src/launcher.ts
+++ src/launcher.ts
@@ -30,7 +30,6 @@ swap-enable: false
 mainnet: true
 full-node: false
 cors-allowed-origins: '*'
-use-postage-snapshot: false
 skip-postage-snapshot: false
 resolver-options: https://cloudflare-eth.com
 data-dir: ${getPath('data-dir')}
